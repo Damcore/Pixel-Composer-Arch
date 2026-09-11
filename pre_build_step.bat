8e8e6a991d5c7834b27f3d9769675bd80a5485ab
Merlin <merlinp2000@gmail.com>
2026-09-11 21:52:53 +0200

    build: remove developer-local path from Windows pre-build hook

--- pre_build_step.bat
+++ pre_build_step.bat
@@ -1,3 +1,4 @@
-py "%~dp0datafiles/Shaders/shader_replace.py"
-py "%~dp0datasrc/update.py"
-python "D:\Project\MakhamDev\LTS-PixelComposer\PixelComposer\pre_run.py"
+@echo off
+py "%~dp0datafiles\Shaders\shader_replace.py"
+py "%~dp0datasrc\update.py"
+py "%~dp0pre_run.py"
